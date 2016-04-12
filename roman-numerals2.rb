cb51653f242d6011dbeb91737a4b3ef6feea5dee
LisaBee224 <lisakbuch@gmail.com>
2016-04-11 21:28:35 -0400

    Roman numerals

---
+++ roman-numerals2.rb
@@ -0,0 +1,37 @@
+$letters = {
+  1000 => "M",
+  500 => "D",
+  100 => "C",
+  50 => "L",
+  10 => "X",
+  5 => "V",
+  1 => "I"
+}
+
+$modern_letters = {
+  "VIIII" => "IX",
+  "IIII" => "IV",
+  "LXXXX" => "XC",
+  "XXXX" => "XL",
+  "DCCCC" => "CM",
+  "CCCC" => "CD"
+}
+
+def convert_to_roman(arabic_number, options = {})
+  number = arabic_number
+  numeral = ""
+    $letters.each do |key,value|
+      numeral += value * (number/key)
+      number -= key * (number/key)
+    end
+
+  if options.has_key?(:modern) && options[:modern] == true
+    $modern_letters.each do |old_nums, new_nums|
+      numeral.sub!(old_nums, new_nums)
+    end
+  end
+
+p numeral
+end
+
+convert_to_roman(44, {:modern => true})
